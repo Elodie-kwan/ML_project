4357aa35d2e4594dc8204baf9fb2ad2c087981f7
Elodie <79649046+Elodie-kwan@users.noreply.github.com>
2022-05-02 16:49:42 +0200

    Data splitting + first attempt NN model

--- scripts/setup.R
+++ scripts/setup.R
@@ -7,7 +7,8 @@ packages <- c(
 "tidyverse", "readxl", #for wrangling
 "dplyr",  "lubridate","DataExplorer","summarytools","Hmisc",
 "ggplot2", "naniar", "patchwork", "FactoMineR",
-"factoextra", "corrplot", "GGally" #for plotting
+"factoextra", "corrplot", "GGally", #for plotting
+"nnet", "caret", "neuralnet" # for Neural Network 
 )
 
 purrr::walk(packages, library, character.only = TRUE)
