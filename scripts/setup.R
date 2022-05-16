482eeff7efdd0b3be2126b314fa19dccb5be3419
ManoelPidoux <manoel.pidoux@gmail.com>
2022-05-16 09:11:17 +0200

    script modification

--- scripts/setup.R
+++ scripts/setup.R
@@ -10,6 +10,6 @@ packages <- c(
 "factoextra", "corrplot", "GGally", #for plotting
 "caret", "neuralnet", "nnet", # for Neural Network 
 "randomForest","naivebayes","plotly",
-"reshape2","cluster", "knitr")
+"reshape2","cluster", "knitr", "terra", "leaflet")
 
 purrr::walk(packages, library, character.only = TRUE)
